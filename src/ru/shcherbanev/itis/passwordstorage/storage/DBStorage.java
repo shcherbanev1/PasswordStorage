58ca803d9c26d998fc1bffbc43740904c8c59b9a
Shcherbanev Konstantin <shcherbanev1@yandex.ru>
2024-01-30 02:07:16 +0300

    DBStorage layout done for future

---
+++ src/ru/shcherbanev/itis/passwordstorage/storage/DBStorage.java
@@ -0,0 +1,16 @@
+package ru.shcherbanev.itis.passwordstorage.storage;
+
+
+// NOT REALESED YET
+public class DBStorage extends AbstractStorage {
+
+    @Override
+    public void readData(String source) {
+        // ...
+    }
+
+    @Override
+    public void saveData() {
+        // ...
+    }
+}
